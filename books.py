6f5eb95687741e61ba1aaa355ede4d90b21405f5
ankitd <dhimanankit262@gmail.com>
2023-01-20 16:18:33 +0530

    add enum methods

--- books.py
+++ books.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from enum import Enum
 app = FastAPI()
 
 Books = {
@@ -30,6 +30,11 @@ Books = {
     },
 }
 
+class Author(str,Enum):
+    auth1 = "Ankit Dhiman"
+    auth2 = "Doraemon"
+    auth3 = "Stephen Hawkings"
+
 @app.get('/')
 def read_all_books():
     return Books
@@ -48,3 +53,11 @@ def read_all_books_alt():
 @app.get('/book/{book_id}')
 def read_book_using_id(book_id:int):
     return Books[f'book{book_id}']
+
+@app.get('/author/{author_name}')
+def get_author_books(author_name:Author):
+    author_books = []
+    for key in Books:
+        if Books[key]['author'] == author_name:
+            author_books.append(Books[key])
+    return author_books
